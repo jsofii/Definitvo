e936a2a036b4c698b173b061c7deb77b213f65c3
Jose7198 <jose719810@hotmail.com>
2016-11-30 04:32:58 -0500

    Metodo registrarPaciente

--- src/sistemasdefinitivo/Funcionario.java
+++ src/sistemasdefinitivo/Funcionario.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,5 +11,85 @@ package sistemasdefinitivo;
  * @author USER
  */
 public class Funcionario {
+    private String ci;
+
+    public String getCi() {
+        return ci;
+    }
+
+    public void setCi(String ci) {
+        this.ci = ci;
+    }
     
+    public Funcionario(String ci){
+        this.ci=ci;
+    }
+    
+    public Paciente registrarPaciente(String nombre,int estado,Medico medico,Habitacion habitacion){
+        Paciente paciente=new Paciente();
+        paciente.setNombre(nombre);
+        paciente.setEstado(estado);
+        Scanner sc=new Scanner(System.in);
+        switch(estado){
+            case 4:
+                System.out.println("El estado del paciente es crítico, es necesario asignarle una habitacion  en Emergencias y un cirujano general");
+                java.util.Date fecha = new Date();
+                Turno turno=new Turno(1000, fecha);
+                paciente.setTurno(turno);                               
+                break;      
+            case 3:
+                System.out.println("El estado del paciente es grave, es necesario asignarle una habitacion  en Emergencias y un cirujano general");
+                java.util.Date fecha1 = new Date();
+                Turno turno1=new Turno(100, fecha1);
+                paciente.setTurno(turno1);
+                System.out.println("Elija un opcion");
+                System.out.println("1. Paciente con traumatismo craneal");
+                System.out.println("2. Paciente sin traumantismo creaneal");
+                break;
+        }
+        paciente.setMedico(medico);
+        paciente.setHabitacion(habitacion);
+        System.out.println("¿El paciente "+nombre+" tiene alergias a algún medicamento?");
+        System.out.println("1.- Ibuprofeno");
+        System.out.println("2.- Penicilina");
+        System.out.println("3.- Anticonvulsivos");
+        System.out.println("4.- Antibióticos");
+        System.out.println("5.-Ninguno");
+        int alergia1=sc.nextInt();
+        String al="";
+        switch(alergia1){
+            case 1:
+                al="Ibuprofeno";
+                break;
+            case 2:
+                al="Penicilina";
+                break;
+            case 3:
+                al="Anticonvulsivo";
+                break;
+            case 4:
+                al="Antibióticos";
+                break;
+            case 5:
+                al="Ninguno";
+                break;                                 
+        }
+        paciente.setAlergias(al);
+        System.out.println("Tiene el paciente seguro?");
+        System.out.println("1. Si");
+        System.out.println("2. No");
+        int cont1=sc.nextInt();                                   
+        switch( cont1){
+            case 1:
+                System.out.println("¿Cuál es la cobertura del seguro?");
+                System.out.println("Ingreselo como termino porcentual en un rango de 0 a 1");
+                double cob=sc.nextDouble();
+                paciente.setSeguro(cob);
+                break;
+            case 2:
+                paciente.setSeguro(cont1);
+                break;
+        }
+        return paciente;
+    }
 }
