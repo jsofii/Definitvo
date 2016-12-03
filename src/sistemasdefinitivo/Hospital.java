259129cf26648f4eabc760c073bfbfe69d5a9c9f
jsofii <sofi-0106@hotmail.com>
2016-12-03 14:42:05 -0500

    Update Hospital.java
    hopital correcion

--- src/sistemasdefinitivo/Hospital.java
+++ src/sistemasdefinitivo/Hospital.java
@@ -9,3 +9,55 @@ package sistemasdefinitivo;
  *
  * @author USER
  */
+public class Hospital {
+    private String nombre;
+    private int capacidad;
+    private String calificacion;
+    private Direccion direccion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(String calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+    
+    public void setHospital(String nombre,String calificacion,int capacidad,String principal,String secundaria,String numero){
+        this.nombre=nombre;
+        this.calificacion=calificacion;
+        this.capacidad=capacidad;
+        this.direccion.setDireccion(principal,secundaria,numero);
+    }
+    
+    public Hospital(){
+        this.nombre="\n-HOSPITAL DE LA ESCUELA POLITÉCNICA NACIONAL-";
+        this.capacidad=30;
+        this.calificacion="A+";
+        this.direccion=new Direccion();
+    }
+}
