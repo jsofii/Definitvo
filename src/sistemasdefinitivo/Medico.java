a55a9c5b57de62bb83ea093c7a5d319c7b184952
jsofii <sofi-0106@hotmail.com>
2016-12-03 14:44:08 -0500

    Update Medico.java

--- src/sistemasdefinitivo/Medico.java
+++ src/sistemasdefinitivo/Medico.java
@@ -9,3 +9,56 @@ package sistemasdefinitivo;
  *
  * @author USER
  */
+public class Medico extends Persona {
+    private String especialidad;
+    private Oficina oficina;
+    private int estado;
+    
+    public int getEstado(){
+        return this.estado;
+    }
+    
+    public String getEstado(int a) {
+        String estado1="";
+        switch(this.estado){
+            case 0:
+                estado1="Desocupado";
+                break;
+            case 1:
+                estado1="Ocupado";
+                break;
+        }
+        return estado1;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+    
+    public String getEspecialidad() {
+        return especialidad;
+    }
+ 
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public Oficina getOficina() {
+        return oficina;
+    }
+
+    public void setOficina(Oficina oficina) {
+        this.oficina = oficina;
+    }
+    
+    public Medico(Oficina oficina){
+        this.especialidad="General";
+        this.oficina=oficina;
+        this.estado=0;
+    }
+    
+    public Medico(){
+        this.estado=0;
+    }
+}
+
