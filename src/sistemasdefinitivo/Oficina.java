5ccf346853340a166442aae7bff79b71943686c4
jsofii <sofi-0106@hotmail.com>
2016-12-03 14:46:43 -0500

    Update Oficina.java

--- src/sistemasdefinitivo/Oficina.java
+++ src/sistemasdefinitivo/Oficina.java
@@ -9,3 +9,42 @@ package sistemasdefinitivo;
  *
  * @author USER
  */
+public class Oficina extends Cuarto{
+    private int tipo;
+    private int estado;
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+    
+    public String getTipo() {
+        String tipo1="";
+        switch(this.tipo){
+            case 0:
+                tipo1="Consultorio";
+                break;
+            case 1:
+                tipo1="Laboratorio";
+                break;
+            case 2:
+                tipo1="Quirófano";
+                break;
+        }
+        return tipo1;
+    }
+    
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+    
+    public Oficina(int numero,Piso piso){
+        super(numero,piso);
+        this.tipo=0;
+        this.tipo=0;
+    }
+    
+}
