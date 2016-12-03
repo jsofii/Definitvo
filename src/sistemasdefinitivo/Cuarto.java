1b1cc5aed8ecf193e3fe1dd3135019bf8a9f398d
jsofii <sofi-0106@hotmail.com>
2016-12-03 14:37:41 -0500

    Update Cuarto.java

--- src/sistemasdefinitivo/Cuarto.java
+++ src/sistemasdefinitivo/Cuarto.java
@@ -9,3 +9,34 @@ package sistemasdefinitivo;
  *
  * @author USER
  */
+public class Cuarto {
+    private int numero;
+    private Piso piso;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Piso getPiso() {
+        return piso;
+    }
+
+    public void setPiso(Piso piso) {
+        this.piso = piso;
+    }
+
+    
+    public Cuarto(int numero,Piso piso){
+        this.numero=numero+100;
+        this.piso=piso;
+    }
+    
+    public Cuarto(){
+        
+    }
+
+}
