4774863753379a5f148650643b5a2fee1d7977e4
jsofii <sofi-0106@hotmail.com>
2016-12-03 14:36:54 -0500

    Update Direccion.java

--- src/sistemasdefinitivo/Direccion.java
+++ src/sistemasdefinitivo/Direccion.java
@@ -9,48 +9,4 @@ package sistemasdefinitivo;
  *
  * @author USER
  */
-public class Direccion {
-    private String principal;
-    private String secundaria;
-    private String numero;
 
-    public String getPrincipal() {
-        return principal;
-    }
-
-    public void setPrincipal(String principal) {
-        this.principal = principal;
-    }
-
-    @Override
-    public String toString() {
-        return principal + " y " + secundaria + ", " + numero;
-    }
-
-    public String getSecundaria() {
-        return secundaria;
-    }
-
-    public void setSecundaria(String secundaria) {
-        this.secundaria = secundaria;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-    
-    public Direccion(){
-        this.principal="Ladron de Guevara";
-        this.secundaria="Quito";
-        this.numero="E11-253";
-    }
-    public void setDireccion(String principal,String secundaria,String numero){
-        this.principal=principal;
-        this.secundaria=secundaria;
-        this.numero=numero;
-    }
-}
