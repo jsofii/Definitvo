b37dfee5f9e18baca18fc06dcc757b5ff453e600
jsofii <sofi-0106@hotmail.com>
2016-12-03 14:40:58 -0500

    Update Habitacion.java
    habitacion corregida errores

--- src/sistemasdefinitivo/Habitacion.java
+++ src/sistemasdefinitivo/Habitacion.java
@@ -9,3 +9,49 @@ package sistemasdefinitivo;
  *
  * @author USER
  */
+public class Habitacion extends Cuarto{
+    private int capacidad;
+    private int estado;
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+    
+    public String getEstado(int a){
+        String estado1="";
+        switch(this.estado){
+            case 0:
+                estado1="vacia";
+                break;
+            case 1:
+                estado1="ocupada";
+                break;
+            case 2:
+                estado1="llena";
+                break;               
+        }
+        return estado1;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+    
+    public Habitacion(int numero,Piso piso){
+        super(numero,piso);
+        this.capacidad=2;
+    }
+    public Habitacion(){
+        this.capacidad=2;
+        this.estado=0;
+        
+    }
+}
